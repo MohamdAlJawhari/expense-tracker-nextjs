823ab61108aa0eb999dcd74e842ef35a04050f16
MohamdAlJawhari <mohamd.aljawhari.com>
2026-09-11 15:58:29 +0300

    feat: implement user authentication and transaction management features

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-const HomePage = () => {
+import Guest from "@/components/Guest"
+import { currentUser } from "@clerk/nextjs/server";
+import AddTransaction from "@/components/AddTransaction";
+
+const HomePage = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest />
+  }
+
   return (
     <main>
       <h1>
-        Expense Tracker
+        Welcome, {user.firstName}
       </h1>
+      <AddTransaction />
     </main>
   );
 }
